661767f4551bf5ea552dfd96d8f5bc9d5285ee48
Eric Binnion <wfhsbinnion@Erics-MacBook-Pro.local>
2012-05-02 17:19:40 -0500

    Initial Commit

---
+++ ticTacToe/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ticTacToe
+//
+//  Created by Eric Binnion on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

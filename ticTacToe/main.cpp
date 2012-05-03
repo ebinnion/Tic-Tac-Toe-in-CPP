c3ba1d7acc6eb968ba93c3e3468e9daf4a287362
Eric Binnion <wfhsbinnion@Erics-MacBook-Pro.local>
2012-05-03 01:10:58 -0500

    First commit

--- ticTacToe/main.cpp
+++ ticTacToe/main.cpp
@@ -1,18 +1,35 @@
-//
-//  main.cpp
-//  ticTacToe
-//
-//  Created by Eric Binnion on 5/2/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+// Tic Tac Toe Game
+// Created by Eric Binnion
+// 2 May 2012
 
 #include <iostream>
+#include "ticTac.h"
 
-int main(int argc, const char * argv[])
+using namespace std;
+
+int main()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ticTac board;
+    string play = "yes";
+    while(play == "yes" || play == "Yes" || play == "YES") {
+        cout << "Hello Human. Let's play a friendly game of Tic-Tac-Toe.";
+        cout << "I have made the first move. I am X's. Play me if you dare!" << endl << endl;
+        board.printBoard();
+        cout << endl << endl;
+        while(!board.checkWin() && !board.boardFull() ){
+            board.dialogue();
+            board.printBoard();
+        }
+        if(board.checkWin())
+            board.printWin();
+        else 
+            board.printDraw();
+        cout << "Would you like to play again? If so, type Yes and press enter." << endl << endl;
+        cin >> play;
+        board.initBoard();
+    }
+    
+    cout << "Bye Bye Now!" << endl;
     return 0;
 }
 
